98abfea9290fe782c5e2cd0bce9800508053003d
Andi Kluge <andi.kluge@mobi4friends.de>
2016-05-06 13:54:04 +0200

    Start Snapshot 8.0.2, use an ExecutorService, read an entire Directory before executing it.

--- src/main/java/net/team33/hashrename/Main.java
+++ src/main/java/net/team33/hashrename/Main.java
@@ -1,9 +1,17 @@
 package net.team33.hashrename;
 
 import java.io.IOException;
-import java.nio.file.*;
-import java.util.Arrays;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.AbstractList;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
 import static java.util.logging.Level.INFO;
@@ -13,20 +21,40 @@ import static net.team33.hashrename.Package.DIGITS;
 @SuppressWarnings("UseOfSystemOutOrSystemErr")
 public final class Main {
 
-    public static final String START_HASH = "[#";
-    public static final String ENDOF_HASH = "]";
+    private static final String START_HASH = "[#";
+    private static final String ENDOF_HASH = "]";
     private static final Logger LOGGER = Logger.getLogger(Main.class.getCanonicalName());
+    private static final ExecutorService EXECUTOR
+            = Executors.newWorkStealingPool(4 + (4 * Runtime.getRuntime().availableProcessors()));
 
     private Main() {
     }
 
     public static void main(final String[] args) throws IOException {
-        rename(new Cursor(args));
+        rename(new AbstractList<Path>() {
+            @Override
+            public Path get(final int index) {
+                return Paths.get(args[index]).toAbsolutePath().normalize();
+            }
+
+            @Override
+            public int size() {
+                return args.length;
+            }
+        });
     }
 
-    private static void rename(final Iterator<Path> paths) {
-        while (paths.hasNext()) {
-            rename(paths.next());
+    private static void rename(final Iterator<Path> iterator) {
+        final List<Path> paths = new LinkedList<>();
+        while (iterator.hasNext()) {
+            paths.add(iterator.next());
+        }
+        rename(paths);
+    }
+
+    private static void rename(final List<Path> paths) {
+        for (final Path path : paths) {
+            EXECUTOR.execute(() -> rename(path));
         }
     }
 
@@ -59,13 +87,13 @@ public final class Main {
                 final String newName = String.format("%s%s%s%s", START_HASH, Hash.from(path), ENDOF_HASH, extension);
                 Files.move(path, path.getParent().resolve(newName));
                 LOGGER.log(INFO, String.format("Moved <%s> to <%s>", path, newName));
-            } catch (IOException e) {
+            } catch (final IOException e) {
                 LOGGER.log(WARNING, String.format("Could not access or rename <%s>", path), e);
             }
         }
     }
 
-    private static boolean isHashName(String name) {
+    private static boolean isHashName(final String name) {
         final int start = name.indexOf(START_HASH);
         if (0 <= start) {
             int i = start + START_HASH.length();
@@ -77,24 +105,5 @@ public final class Main {
             }
         }
         return false;
-    }
-
-    private static class Cursor implements Iterator<Path> {
-
-        private final Iterator<String> backing;
-
-        private Cursor(String[] paths) {
-            this.backing = Arrays.asList(paths).iterator();
-        }
-
-        @Override
-        public final boolean hasNext() {
-            return backing.hasNext();
-        }
-
-        @Override
-        public final Path next() {
-            return Paths.get(backing.next()).toAbsolutePath().normalize();
-        }
     }
 }
