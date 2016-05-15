90b30428493e1621f23734d9f80e0f965bd227fa
Andi Kluge <andi.kluge@mobi4friends.de>
2016-05-15 22:39:45 +0200

    sieve to a specified directory

--- src/main/java/net/team33/hashrename/Main.java
+++ src/main/java/net/team33/hashrename/Main.java
@@ -6,108 +6,85 @@ import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.AbstractList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
 
 import static java.util.logging.Level.INFO;
 import static java.util.logging.Level.WARNING;
-import static net.team33.hashrename.Package.DIGITS;
 
 @SuppressWarnings("UseOfSystemOutOrSystemErr")
 public final class Main {
 
-    private static final String START_HASH = "[#";
-    private static final String ENDOF_HASH = "]";
     private static final Logger LOGGER
             = Logger.getLogger(Main.class.getCanonicalName());
     private static final ExecutorService EXECUTOR
             = Executors.newWorkStealingPool(4 + (4 * Runtime.getRuntime().availableProcessors()));
     private static final Activity ACTIVITY
             = new Activity();
+    private static final AtomicInteger MOVED = new AtomicInteger();
+    private static final AtomicInteger FAILED = new AtomicInteger();
 
     private Main() {
     }
 
     public static void main(final String[] args) throws IOException, InterruptedException {
-        rename(new AbstractList<Path>() {
-            @Override
-            public Path get(final int index) {
-                return Paths.get(args[index]).toAbsolutePath().normalize();
-            }
-
-            @Override
-            public int size() {
-                return args.length;
-            }
-        });
+        rename(
+                Paths.get(args[0]).toAbsolutePath().normalize(), Paths.get(args[1]).toAbsolutePath().normalize()
+        );
         ACTIVITY.join();
+        LOGGER.info("MOVED:  " + MOVED + " files");
+        LOGGER.info("FAILED: " + FAILED + " files");
     }
 
-    private static void rename(final Iterator<Path> iterator) {
+    private static void rename(final Path sievedRoot, final Iterator<Path> iterator) {
         final List<Path> paths = new LinkedList<>();
         while (iterator.hasNext()) {
             paths.add(iterator.next());
         }
-        rename(paths);
+        rename(sievedRoot, paths);
     }
 
-    private static void rename(final List<Path> paths) {
+    private static void rename(final Path sievedRoot, final List<Path> paths) {
         for (final Path path : paths) {
-            EXECUTOR.execute(ACTIVITY.add(() -> rename(path)));
+            EXECUTOR.execute(ACTIVITY.add(() -> rename(path, sievedRoot)));
         }
     }
 
-    private static void rename(final Path path) {
+    private static void rename(final Path path, final Path sievedRoot) {
         if (Files.isRegularFile(path, LinkOption.NOFOLLOW_LINKS)) {
-            renameRegularFile(path);
+            renameRegularFile(path, sievedRoot);
         } else if (Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)) {
-            renameContent(path);
+            renameContent(path, sievedRoot);
         } else {
             LOGGER.log(INFO, String.format("Not a regular file or directory: <%s>", path));
         }
     }
 
-    private static void renameContent(final Path path) {
+    private static void renameContent(final Path path, final Path sievedRoot) {
         try (DirectoryStream<Path> paths = Files.newDirectoryStream(path)) {
-            rename(paths.iterator());
+            if (!path.equals(sievedRoot)) {
+                rename(sievedRoot, paths.iterator());
+            }
         } catch (final IOException e) {
             LOGGER.log(WARNING, String.format("Could not read directory <%s>", path), e);
         }
     }
 
-    private static void renameRegularFile(final Path path) {
-        final String oldName = path.getFileName().toString();
-        if (isHashName(oldName)) {
-            LOGGER.log(INFO, String.format("Nothing to do: <%s>", path));
-        } else {
-            final int extStart = oldName.lastIndexOf('.');
-            final String extension = (0 > extStart) ? "" : oldName.substring(extStart);
-            try {
-                final String newName = String.format("%s%s%s%s", START_HASH, Hash.from(path), ENDOF_HASH, extension);
-                Files.move(path, path.getParent().resolve(newName));
-                LOGGER.log(INFO, String.format("Moved <%s> to <%s>", path, newName));
-            } catch (final IOException e) {
-                LOGGER.log(WARNING, String.format("Could not access or rename <%s>", path), e);
-            }
+    private static void renameRegularFile(final Path path, final Path sievedRoot) {
+        try {
+            final Naming naming = new Naming(path);
+            final Path target = naming.resolve(sievedRoot);
+            Files.move(path, target);
+            MOVED.addAndGet(1);
+            LOGGER.log(INFO, String.format("Moved <%s>\n\t-> <%s>", path, target));
+        } catch (final IOException e) {
+            FAILED.addAndGet(1);
+            LOGGER.log(WARNING, String.format("Could not access or rename <%s>", path), e);
         }
-    }
-
-    private static boolean isHashName(final String name) {
-        final int start = name.indexOf(START_HASH);
-        if (0 <= start) {
-            int i = start + START_HASH.length();
-            while (DIGITS.indexOf(name.charAt(i)) >= 0) {
-                i += 1;
-            }
-            if (name.substring(i).startsWith(ENDOF_HASH)) {
-                return true;
-            }
-        }
-        return false;
     }
 }
--- src/main/java/net/team33/hashrename/Package.java
+++ src/main/java/net/team33/hashrename/Package.java
@@ -3,4 +3,7 @@ package net.team33.hashrename;
 @SuppressWarnings("SpellCheckingInspection")
 public class Package {
     static final String DIGITS = "0123456789abcdefghijklmnopqrstuvwxyz";
+    static final String LEADING = "0000";
+    static final String START_HASH = "[#";
+    static final String ENDOF_HASH = "]";
 }
